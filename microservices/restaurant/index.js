cd90d8ab1975a66521cbf46756f21831c97c1cb6
Melvin MENANTEAU <melvin.menanteau@gmail.com>
2025-03-19 22:57:09 +0100

    feat (micro-service restaurant) : Exemple d'API pour micro-service

--- microservices/restaurant/index.js
+++ microservices/restaurant/index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3101;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+/* Middleware pour gérer les CORS */
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200); // Répondre immédiatement aux preflight requests (requête vérfiant les CORS)
+    }
+
+    next();
+});
+
+app.get('/restaurants/get', (req, res) => {
     setTimeout(() => {
-        res.send('Hello World!');
-    }, 5000);
-})
+        res.send({
+            code: 200,
+            ok: true,
+            message: 'Micro-service gérant les restaurants'
+        });
+    }, 1000);
+});
 
 app.listen(port, () => {
     console.log(`Lancement de du micro-service gérant les restaurants sur le port : ${port}`);
-})
+});
