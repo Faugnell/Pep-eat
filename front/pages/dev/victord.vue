2c77a5a88012153d30ec24a05e3bf445d282aa96
VictorDexes <125280766+VictorDexes@users.noreply.github.com>
2025-03-24 14:27:03 +0100

    Caroussel restaurants & cat

--- front/pages/dev/victord.vue
+++ front/pages/dev/victord.vue
@@ -6,21 +6,30 @@
 /* -------------------------------------------------------------------------
 ------------------------------- VARIABLES ----------------------------------
 ------------------------------------------------------------------------- */
-const items = [
-  '/icon/cat/American.png',
-  '/icon/cat/Asian.png',
-  '/icon/cat/BBQ.png',
-  '/icon/cat/Burgers.png',
-  '/icon/cat/BubbleTea.png',
-  '/icon/cat/Chinese.png',
-  '/icon/cat/Hawaiian.png',
-  '/icon/cat/IceCream.png',
-  '/icon/cat/Grocery_v2.png',
-  '/icon/cat/FastFood.png',
-  '/icon/cat/Halal.png',
-  '/icon/cat/Japanese.png',
-  '/icon/cat/Sandwich.png',
-  '/icon/cat/Pizza.png',
+const categories = [
+  '/icons/cat/American.png',
+  '/icons/cat/Asian.png',
+  '/icons/cat/BBQ.png',
+  '/icons/cat/Burgers.png',
+  '/icons/cat/BubbleTea.png',
+  '/icons/cat/Chinese.png',
+  '/icons/cat/Hawaiian.png',
+  '/icons/cat/IceCream.png',
+  '/icons/cat/Grocery_v2.png',
+  '/icons/cat/FastFood.png',
+  '/icons/cat/Halal.png',
+  '/icons/cat/Japanese.png',
+  '/icons/cat/Sandwich.png',
+  '/icons/cat/Pizza.png',
+]
+
+const restaurants = [
+    'https://picsum.photos/500/300?random=1',
+    'https://picsum.photos/500/300?random=2',
+    'https://picsum.photos/500/300?random=3',
+    'https://picsum.photos/500/300?random=4',
+    'https://picsum.photos/500/300?random=5',
+    'https://picsum.photos/500/300?random=6',
 ]
 
 /* -------------------------------------------------------------------------
@@ -39,38 +48,57 @@ const items = [
 <template>
     <div class="flex flex-col">
         <div class="flex w-full justify-center px-24">
-            <UCarousel v-slot="{ item }" arrows :items="items" :ui="{ item: 'basis-1/10' }" class="max-w mx-auto">
-                 <NuxtImg :src="item    " width="320" height="320" class="rounded-lg" />
+            <UCarousel
+            v-slot="{ item: categorie }"
+            arrows :items="categories"
+            :next="{variant:'ghost'}"
+            :prev="{variant:'ghost'}"
+            prev-icon="i-material-symbols-light:arrow-back-ios-new"
+            next-icon="i-material-symbols-light:arrow-forward-ios"
+            :ui="{ item: 'basis-1/10' }"
+            class="max-w mx-auto">
+                 <NuxtImg :src="categorie" width="320" height="320" class="rounded-sm" />
             </UCarousel>
         </div> 
         <div class="flex flex-col w-full">
             <div class="flex w-full">
                 <h2 class="font-semibold">Offres prés de chez vous</h2>
             </div>
-            <!-- <div class="flex w-full">
-                <UCarousel v-slot="{ item }" arrows :items="items" :ui="{ item: 'basis-1/3' }" class="max-w mx-auto">
-                    <NuxtImg :src="item    " width="320" height="320" class="rounded-lg" />
-                </UCarousel>
-            </div> -->
         </div>
-        <div>
-            <UCarousel v-slot="{ item }" arrows :items="items" :ui="{ item: 'basis-1/3' }" class="max-w mx-auto p-10">
-                <div class="p-1">
-                    <UCard>
-                        <NuxtImg :src="item " width="320" height="320" class="rounded-lg" />
+        <div class="flex w-full justify-center px-24">
+            <UCarousel
+            v-slot="{ item: restaurant }"
+            arrows
+            :next="{variant:'ghost'}"
+            :prev="{variant:'ghost'}"
+            prev-icon="i-material-symbols-light:arrow-back-ios-new"
+            next-icon="i-material-symbols-light:arrow-forward-ios"
+            :items="restaurants"
+            :ui="{ item: 'basis-1/3', prev: '', next: '' }" class="mx-auto">
+                <div class="px-2 py-1">
+                    <UCard class="drop-shadow-sm">
+                        <NuxtImg
+                        :src="restaurant"
+                        fit="cover"
+                        width="500"
+                        height="325"
+                        class="rounded-lg" />
                         <template #footer>
-                            <div>
+                            <div class="flex w-full">
                                 <h3>Miam miam</h3>
-                            </div>
-                            <div>
-                                <UIcon name="i-material-symbols-light-star" class="size-5" /> 
-                                <h3 icon="i-material-symbols-light:star">Miam miam</h3>
+                                <div class="flex ml-auto">
+                                    <div class="flex flex-col justify-center">
+                                        <h3>4.1</h3>
+                                    </div>
+                                    <div class="flex flex-col justify-center">
+                                        <UIcon name="i-material-symbols-light:star" size="25" />
+                                    </div>
+                                </div>
                             </div>
                         </template>
                     </UCard>
                 </div>
             </UCarousel>
-           
         </div>
     </div>
 </template>
