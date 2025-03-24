97141488bbe17f5ff6c756fa81430b84570a144e
Faugnell <vic.petit98@gmail.com>
2025-03-24 22:02:34 +0100

    feat: Changement de police pour Zain

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,30 +2,36 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-				modules: ['@nuxt/icon', '@nuxt/ui', '@nuxt/image'],
-				app: {
-								head: {
-												title: 'Pep\'eat',
-												htmlAttrs: {
-																lang: 'fr'
-												}
-								}
-				},
-				css: [
-								'~/assets/css/main.css'
-				],
-				vite: {
-								plugins: [
-												tailwindcss()
-								]
-				},
-				ui: {
-								theme: {
-												colors: ['primary', 'secondary', 'tertiary', 'info', 'success', 'warning', 'error']
-								}
-				},
-				compatibilityDate: '2024-11-01',
-				devtools: {
-								enabled: true
+	modules: ['@nuxt/icon', '@nuxt/ui', '@nuxt/image'],
+	app: {
+		head: {
+			title: 'Pep\'eat',
+			htmlAttrs: {
+				lang: 'fr'
+			},
+			link: [
+				{
+					rel: 'stylesheet',
+					href: 'https://fonts.googleapis.com/css2?family=Zain&display=swap'
 				}
+			]
+		}
+	},
+	css: [
+		'~/assets/css/main.css'
+	],
+	vite: {
+		plugins: [
+			tailwindcss()
+		]
+	},
+	ui: {
+		theme: {
+			colors: ['primary', 'secondary', 'tertiary', 'info', 'success', 'warning', 'error']
+		}
+	},
+	compatibilityDate: '2024-11-01',
+	devtools: {
+		enabled: true
+	}
 })
