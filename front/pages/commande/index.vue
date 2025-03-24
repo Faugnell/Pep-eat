579527eecbe5a33ded9970bcffd8289ad2370ed4
Charlyhno-eng <charly7902@gmail.com>
2025-03-24 19:34:36 +0100

    commit afin de récupérer la master pour le livrable

---
+++ front/pages/commande/index.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import HeaderPepeat from "~/components/common/HeaderPepeat.vue";
+
+type Items = {
+    title: "Adresse" | "Expédition" | "Finalisation";
+    description: string;
+    icon?: string;
+}
+
+const items: Items[] = [
+    { title: "Adresse", description: "Ajouter votre adresse", icon: "i-lucide-house" },
+    { title: "Expédition", description: "Choisisser votre méthode d'expédition", icon: "i-lucide-truck" },
+    { title: "Finalisation", description: "Confirmer votre commande" }
+]
+</script>
+
+<template>
+    <HeaderPepeat />
+
+    <UStepper ref="stepper" :items="items" class="w-full">
+        <template #content="{ item }">
+            <div class="flex justify-center">
+                <UCard class="w-4/5">
+                    <template v-if="item?.title === 'Adresse'">
+                        <p>Adresse</p>
+                    </template>
+
+                    <template v-else-if="item?.title === 'Expédition'">
+                        <p>Expédition</p>
+                    </template>
+
+                    <!-- Page de finalisation -->
+                    <template v-else-if="item?.title === 'Finalisation'">
+                        <p>Panier</p>
+
+                        <UCard>
+                            <USeparator class="my-4" />
+
+                        </UCard>
+
+                        <!-- Footer pour payer -->
+                        <div class="flex justify-around w-full h-[7vh] min-h-10 fixed bottom-0">
+                            <p>
+                                Total de la commande :
+                            </p>
+                            <UTextarea placeholder="Ajouter une note (ex: pas d'oignon)" />
+                            <div>
+                                <UButton>Continuer les achats</UButton>
+                                <UButton>Valider la commande</UButton>
+                            </div>
+                        </div>
+
+                    </template>
+                </UCard>
+            </div>
+        </template>
+    </UStepper>
+</template>
