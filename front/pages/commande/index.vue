2c5faf8ed56348f6a023180255bf8574ea7d1af4
Charlyhno-eng <charly7902@gmail.com>
2025-03-25 08:28:42 +0100

    Commit pour récupérer la partie de Victor P

--- front/pages/commande/index.vue
+++ front/pages/commande/index.vue
@@ -39,17 +39,18 @@ const items: Items[] = [
                         </UCard>
 
                         <!-- Footer pour payer -->
-                        <div class="flex justify-around w-full h-[7vh] min-h-10 fixed bottom-0">
-                            <p>
-                                Total de la commande :
-                            </p>
-                            <UTextarea placeholder="Ajouter une note (ex: pas d'oignon)" />
-                            <div>
-                                <UButton>Continuer les achats</UButton>
-                                <UButton>Valider la commande</UButton>
+                        <div class="fixed bottom-0 left-0 w-full bg-gray-50 p-4 shadow-lg flex flex-col items-center">
+                            <p class="text-lg font-semibold text-left w-[60%] mb-4">Total de la commande : x€ </p>
+
+                            <div class="flex flex-col items-center w-[60%] space-y-3">
+                                <UTextarea :rows="8" placeholder="Ajouter une note (ex: pas d'oignon)" class="w-full" />
+
+                                <div class="flex justify-between w-full">
+                                    <UButton class="w-1/2 mr-2" color="primary">Continuer les achats</UButton>
+                                    <UButton class="w-1/2 ml-2" color="primary">Valider la commande</UButton>
+                                </div>
                             </div>
                         </div>
-
                     </template>
                 </UCard>
             </div>
