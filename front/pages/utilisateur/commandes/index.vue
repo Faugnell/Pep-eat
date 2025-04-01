53479c51bdf9d70bf7adae9bd1c3b40a70783972
VictorDexes <125280766+VictorDexes@users.noreply.github.com>
2025-04-01 10:00:59 +0200

    Consultation commandes finit et responsive

--- front/pages/utilisateur/commandes/index.vue
+++ front/pages/utilisateur/commandes/index.vue
@@ -31,44 +31,47 @@ const {data} = await useFetch(`http://localhost:3102/commandes/user/${user_id}`,
 
 <template>
     <HeaderPepeat />
-    <div v-if="data == null" class="flex flex-col px-8 min-h-screen">
-        <p>y a pas wesh</p>
-    </div>
-    <div v-else class="flex flex-col px-8 min-h-screen">
-        <div class="flex justify-center">
-            <div v-if="data.data.length !==0" class="flex flex-col px-2.5 py-2.5 w-7/10 border-1 rounded-sm border-gray-300">
-                <template  v-for="(commande, index) in data.data">
-                    <USeparator v-if="index !== 0"/>
-                    <NuxtLink :to="`commandes/${commande._id}`">
-                        <div class="group flex justify-between p-2.5 hover:bg-gray-500/10 transition-all hover:cursor-pointer active:bg-gray-500/5 rounded-lg">
-                            <div class="flex w-full flex-col justify-center">
-                                <div class="flex w-full justify-between">
-                                    <div class="flex justify-left">
-                                        <div class="flex-col h-full w-16 py-1 justify-center">
-                                            <UBadge class="rounded-full size-full flex justify-center text-xl">{{ commande.restaurant_data.nom[0] }}</UBadge>
+    <div class="min-h-screen">
+        <div v-if="data == null" class="flex flex-col px-8 min-h-screen">
+            <p>y a pas wesh</p>
+        </div>
+        <div v-else class="flex flex-col px-8 min-h-screen">
+            <p v-if="data.data.length !==0" class="py-3 w-full text-center text-xl">Vos commandes:</p>
+            <div class="flex justify-center">
+                <div v-if="data.data.length !==0" class="flex flex-col px-2.5 py-2.5 w-7/10 border-1 rounded-sm border-gray-300">
+                    <template  v-for="(commande, index) in data.data">
+                        <USeparator v-if="index !== 0"/>
+                        <NuxtLink :to="`commandes/${commande._id}`">
+                            <div class="group flex justify-between p-2.5 hover:bg-gray-500/10 transition-all hover:cursor-pointer active:bg-gray-500/5 rounded-lg">
+                                <div class="flex w-full flex-col justify-center">
+                                    <div class="flex w-full justify-between">
+                                        <div class="flex justify-left">
+                                            <div class="flex-col h-full w-16 py-1 justify-center align-middle">
+                                                <UBadge class="rounded-full size:25 md:size-full flex justify-center text-xl">{{ commande.restaurant_data.nom[0] }}</UBadge>
+                                            </div>
+                                            <div class="flex pl-3 flex-col">
+                                                <p class="text-lg">{{commande.restaurant_data.nom}}</p>
+                                                <p class="text-sm">{{commande.status}}</p>
+                                                <p class="text-md">{{`${commande.price['$numberDecimal'].replace('.',',')}€ • ${new Date(commande.date).toLocaleString()}`}}</p>
+                                            </div>
                                         </div>
-                                        <div class="flex pl-3 flex-col">
-                                            <p class="text-lg">{{commande.restaurant_data.nom}}</p>
-                                            <p class="text-sm">{{commande.status}}</p>
-                                            <p class="text-md">{{`${commande.price['$numberDecimal'].replace('.',',')}€ • ${new Date(commande.date).toLocaleString()}`}}</p>
-                                        </div>
-                                    </div>
-                                    <div class="flex justify-right">
-                                        <div class="flex flex-col justify-center">
-                                            <UIcon name="i-material-symbols-light:arrow-forward-ios" class="text-primary-500 group-hover:text-primary-600" size="28" />
+                                        <div class="flex justify-right">
+                                            <div class="flex flex-col justify-center">
+                                                <UIcon name="i-material-symbols-light:arrow-forward-ios" class="text-primary-500 group-hover:text-primary-600" size="28" />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </NuxtLink>
-                </template>
-            </div>
-            <div v-else class="flex flex-col py-5 w-6/10">
-                <h1 class="text-2xl w-full text-center">Aucune commande</h1>
-                <USeparator class="py-5"/>
-                <p class="text-lg text-center">Faite votre première commande sur Pep'eat et régalez-vous !</p>
-                <p class="text-md text-center">Accéder à notre large catalogue de restaurant en <NuxtLink class="text-primary-500" to="/">cliquant ici ! <UIcon name="i-ic:round-launch" size="10"/></NuxtLink></p>
+                        </NuxtLink>
+                    </template>
+                </div>
+                <div v-else class="flex flex-col py-5 w-6/10">
+                    <h1 class="text-2xl w-full text-center">Aucune commande</h1>
+                    <USeparator class="py-5"/>
+                    <p class="text-lg text-center">Faite votre première commande sur Pep'eat et régalez-vous !</p>
+                    <p class="text-md text-center">Accéder à notre large catalogue de restaurant en <NuxtLink class="text-primary-500" to="/">cliquant ici ! <UIcon name="i-ic:round-launch" size="10"/></NuxtLink></p>
+                </div>
             </div>
         </div>
     </div>
