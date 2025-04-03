6a05d40de5f00a70ae48e220b9654f18a89e0281
Melvin MENANTEAU <melvin.menanteau@gmail.com>
2025-04-03 21:37:11 +0200

    fix (API Restaurateurs) : La méthode post, pour créer un restaurant, ne peux pas avoir l'id du restaurant en paramètre

--- front/server/api/restaurants/index.post.ts
+++ front/server/api/restaurants/index.post.ts
@@ -1,13 +1,8 @@
 import { H3Event } from "h3";
 import { Response } from "~/utils/types/Response";
 import { Restaurant } from "~/utils/types/Restaurant";
-import { buildErrorResponse } from "~/utils/responseBuilder";
 
 export default defineEventHandler(async (event : H3Event) => {
-    const id = getRouterParam(event, 'id');
-
-    if (!id) return buildErrorResponse(null, 400, `L'ID du restaurant est requis`);
-
     const body = await readBody(event);
 
     try {
