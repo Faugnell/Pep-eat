40cbf7b4a1b1459884e25c601727d6c998cd3bfc
Melvin MENANTEAU <melvin.menanteau@gmail.com>
2025-04-03 16:29:33 +0200

    fix ci/cd

--- microservices/restaurant/controllers/restaurants.ts
+++ microservices/restaurant/controllers/restaurants.ts
@@ -12,6 +12,8 @@ const Restaurant = require("../models/restaurants");
  */
 export async function find(req:Request, res:Response) {
 	console.log(`Find restaurant - ${req.params.id}`)
+	console.log(process.env);
+	console.log(process.env.RESTAURANT_API);
 
 	const id = req.params.id;
 
