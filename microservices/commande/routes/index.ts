87bbf87b8a0c7014a8b72facfcfc907faf2dfd5e
VictorDexes <125280766+VictorDexes@users.noreply.github.com>
2025-04-05 15:40:13 +0200

    CRUD Panier

--- microservices/commande/routes/index.ts
+++ microservices/commande/routes/index.ts
@@ -8,8 +8,10 @@ const jsonParser = bodyParser.json()
 const router = express.Router();
 
 const commandesRoutes = require("./commandes");
+const paniersRoutes = require("./paniers");
 
 router.use("/commandes", jsonParser, commandesRoutes);
+router.use("/paniers", jsonParser, paniersRoutes);
 
 router.get("/", (req:Request, res:Response)=>{
     res.status(200).send("<h1>zbraa</h1>")
