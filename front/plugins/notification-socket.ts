c6f43b99465a55e5ccace2f7374b7c31e6dbc874
Melvin MENANTEAU <melvin.menanteau@gmail.com>
2025-04-08 10:11:13 +0200

    test déploiement sur le microservice des notifications

--- front/plugins/notification-socket.ts
+++ front/plugins/notification-socket.ts
@@ -1,6 +1,9 @@
 import { io } from 'socket.io-client';
 
 export default defineNuxtPlugin(() => {
+    console.log('Notification socket plugin loaded');
+    console.log(useRuntimeConfig());
+
     const socket = io(`http://${process.env.API_NOTIFICATION_SERVICE_HOST}:${process.env.API_NOTIFICATION_SERVICE_PORT}`);
 
     console.log(`Connecting to notification socket at http://${process.env.API_NOTIFICATION_SERVICE_HOST}:${process.env.API_NOTIFICATION_SERVICE_PORT}`);
