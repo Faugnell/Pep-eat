3924fe17b7ad9bfd43ab78203dc9f583085fa470
Melvin MENANTEAU <melvin.menanteau@gmail.com>
2025-04-08 11:43:48 +0200

    fix ci/cd notifications

--- front/server/api/utils/socketUrl.get.ts
+++ front/server/api/utils/socketUrl.get.ts
@@ -9,8 +9,8 @@ export default defineEventHandler(async (event : H3Event) => {
             code: 200,
             ok: true,
             data: {
-                serviceHost: process.env.API_NOTIFICATION_SERVICE_HOST,
-                servicePort: process.env.API_NOTIFICATION_SERVICE_PORT,
+                serviceHost: process.env.PUBLIC_NOTIFICATION_SERVICE_HOST,
+                servicePort: process.env.PUBLIC_NOTIFICATION_SERVICE_PORT,
             }
         } as Response<any>;
     } catch (error) {
