79e3176c577a603faaaa300d0264e6d4f405b1f4
Charlyhno-eng <charly7902@gmail.com>
2025-04-08 11:20:42 +0200

    Récupération du main suite à un bug lié aux notifs

---
+++ microservices/commande/models/log.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+    const logSchema = new mongoose.Schema({
+    timestamp: { type: Date, default: Date.now },
+    service: { type: String, required: true },
+    code: { type: Number, required: true },
+    ok: { type: Boolean, required: true },
+    message: { type: String, required: true },
+});
+
+export const LogModel = mongoose.model('Log', logSchema);
--- microservices/restaurant/utils/responseBuilder.ts
+++ microservices/restaurant/utils/responseBuilder.ts
@@ -1,4 +1,7 @@
-export function buildSuccessResponse(data:[unknown], code = 200, message = 'Success') {
+import { logResponse } from './logger';
+
+export async function buildSuccessResponse(data: [unknown], code = 200, message = 'Success', service = 'restaurant') {
+    await logResponse({ service, code, ok: true, message });
     return {
         code: code,
         ok: true,
@@ -7,7 +10,8 @@ export function buildSuccessResponse(data:[unknown], code = 200, message = 'Succ
     }
 }
 
-export function buildErrorResponse(error:any, code = 500, message = 'Internal Server Error') {
+export async function buildErrorResponse(error: any, code = 500, message = 'Internal Server Error', service = 'restaurant') {
+    await logResponse({ service, code, ok: true, message });
     return {
         code: code,
         ok: false,
