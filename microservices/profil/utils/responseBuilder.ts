43ecfbbc010c16f7e5ab8f7938734ac0a6ddb270
Charlyhno-eng <charly7902@gmail.com>
2025-04-09 09:13:11 +0200

    Récupération de la main

--- microservices/profil/utils/responseBuilder.ts
+++ microservices/profil/utils/responseBuilder.ts
@@ -1,7 +1,7 @@
-// import { logResponse } from './logger';
+import { logResponse } from './logger';
 
 export function buildSuccessResponse(data: [unknown], code = 200, message = 'Success', service = 'profil') {
-    // await logResponse({ service, code, ok: true, message });
+    logResponse({ service, code, ok: true, message });
     return {
         code: code,
         ok: true,
@@ -11,7 +11,7 @@ export function buildSuccessResponse(data: [unknown], code = 200, message = 'Suc
 }
 
 export function buildErrorResponse(error: unknown, code = 500, message = 'Internal Server Error', service = 'profil') {
-    // await logResponse({ service, code, ok: false, message });
+    logResponse({ service, code, ok: false, message });
     return {
         code: code,
         ok: false,
