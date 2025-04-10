04f73a8267fc4d82d4d92c131983a85b3535a83d
Melvin MENANTEAU <melvin.menanteau@gmail.com>
2025-04-10 19:03:31 +0200

    fix (API Restaurant) : Réintroduction de la route API permettant de récupérer les restaurants d'un utilisateur qui avait été supprimée par erreur

--- front/server/api/restaurants/user/[id].get.ts
+++ front/server/api/restaurants/user/[id].get.ts
@@ -6,10 +6,10 @@ import { buildErrorResponse } from "~/utils/responseBuilder";
 export default defineEventHandler(async (event : H3Event) => {
     const id = getRouterParam(event, 'id');
 
-    if (!id) return buildErrorResponse(null, 400, `Le filtre est requis`);
+    if (!id) return buildErrorResponse(null, 400, `L'ID de l'utilisateur est requis`);
 
     try {
-        const response = await $fetch<Response<Restaurant[]>>(`http://${process.env.API_RESTAURANT_SERVICE_HOST}:${process.env.API_RESTAURANT_SERVICE_PORT}/restaurants/filter/${id}`, {
+        const response = await $fetch<Response<Restaurant[]>>(`http://${process.env.API_RESTAURANT_SERVICE_HOST}:${process.env.API_RESTAURANT_SERVICE_PORT}/restaurants/user/${id}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
